8117b1167802ae77d4055a77dd9f6d4a5d33144b
Joanna Skowron <j.skowron89@gmail.com>
2021-05-22 19:40:09 +0200

    Fix countries service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { CountriesService } from './countries/countries.service';
-import {CitiesService} from './cities/cities.service';
+import { CitiesService } from './cities/cities.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor(private countriesService: CountriesService, private citiesService: CitiesService){}
+  constructor(
+    private countriesService: CountriesService,
+    private citiesService: CitiesService
+  ) {}
   title = 'Angular-Countries-Project';
 
   ngOnInit() {
     this.countriesService.loadCountries();
-    this.citiesService.loadCities()
+    this.citiesService.loadCities();
   }
 }
--- src/app/countries/countries.component.ts
+++ src/app/countries/countries.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
 import { CountryI } from 'src/typings';
 import { CountriesService } from './countries.service';
 
@@ -9,21 +9,9 @@ import { CountriesService } from './countries.service';
   styleUrls: ['./countries.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CountriesComponent implements OnInit, OnDestroy {
-  countries: CountryI[];
-  subscription: Subscription;
+export class CountriesComponent {
+  countries$: Observable<CountryI[]> = this.countriesService.state$;
   
   constructor(private countriesService: CountriesService) {
-    this.countries = [];
-  }
-
-  ngOnInit(): void {
-    this.subscription = this.countriesService
-      .getCountries()
-      .subscribe((data) => (this.countries = data));
-  }
-
-  ngOnDestroy(): void{
-    this.subscription.unsubscribe();
   }
 }
