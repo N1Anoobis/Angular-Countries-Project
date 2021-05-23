660cdcf84e76de0d7be18a7ad363998f1790b2b2
Joanna Skowron <j.skowron89@gmail.com>
2021-05-23 20:11:00 +0200

    Country component & service

--- src/app/countries/country/country.component.ts
+++ src/app/countries/country/country.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  OnDestroy,
+} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+import { CountriesService } from 'src/app/services/countries.service';
+import { CountryI } from 'src/typings';
 
 @Component({
   selector: 'app-country',
   templateUrl: './country.component.html',
   styleUrls: ['./country.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CountryComponent implements OnInit {
+export class CountryComponent implements OnInit, OnDestroy {
+  private paramsSubscription: Subscription;
+  selectedCountry$: Observable<CountryI> =
+    this.countriesService.selectedCountry$;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private countriesService: CountriesService
+  ) {}
 
   ngOnInit(): void {
+    this.paramsSubscription = this.route.params.subscribe((params) =>
+      this.countriesService.setCountryId(params['id'])
+    );
   }
 
+  ngOnDestroy(): void {
+    this.paramsSubscription.unsubscribe();
+  }
 }
--- src/app/services/countries.service.ts
+++ src/app/services/countries.service.ts
@@ -2,17 +2,22 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { CountryI } from '../../typings';
-import { distinctUntilChanged, map } from 'rxjs/operators';
+import {
+  distinctUntilChanged,
+  map,
+} from 'rxjs/operators';
 
 export interface CountriesState {
   isLoading: boolean;
   countries: CountryI[];
+  countryId: string;
   error: string;
 }
 
 const initialState: CountriesState = {
   isLoading: false,
   countries: [],
+  countryId: '',
   error: '',
 };
 
@@ -20,24 +25,48 @@ const initialState: CountriesState = {
 export class CountriesService {
   private readonly subject: BehaviorSubject<CountriesState> =
     new BehaviorSubject(initialState);
+
   public readonly state$: Observable<CountriesState> =
     this.subject.asObservable();
+
   public readonly countries$: Observable<CountryI[]> = this.state$.pipe(
     map((state) => {
       return state.countries;
     }),
     distinctUntilChanged()
   );
+
+  public readonly countryId$: Observable<string> = this.state$.pipe(
+    map((state) => state.countryId, distinctUntilChanged())
+  );
+
+  public readonly selectedCountry$: Observable<CountryI> = this.state$.pipe(
+    map(({countries, countryId}) => countries.find((country) => country.id === countryId)),
+    distinctUntilChanged()
+  );
+
   constructor(private http: HttpClient) {}
 
   loadCountries(): void {
-    const request = this.http.request('get', 'http://localhost:3000/countries');
-    request.subscribe((load) =>
+    const request$ = this.http.request('get', 'http://localhost:3000/countries');
+    request$.subscribe((load) =>
       this.subject.next({
         isLoading: false,
         countries: load,
         error: '',
       } as CountriesState)
     );
+  }
+
+  setState(callback: (state: CountriesState) => CountriesState): void {
+    const currentState = this.subject.getValue();
+    this.subject.next(callback(currentState));
+  }
+
+  setCountryId(id: string): void {
+    const callback: (state: CountriesState) => CountriesState = (state) => {
+      return { ...state, countryId: id };
+    };
+    this.setState(callback);
   }
 }
